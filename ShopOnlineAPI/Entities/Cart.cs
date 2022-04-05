1305fb58ff5ae7c65c86c022d3519be7750841b4
emilio barrera <barrera_emilio@yahoo.es>
2022-04-06 00:03:56 +0100

    Add project files.

---
+++ ShopOnlineAPI/Entities/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnlineAPI.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ ShopOnlineAPI/Entities/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnlineAPI.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
